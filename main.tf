db38003e0ea58c92ee7f77767323dec1cbd1ee52
oebinu <oebinu2021@gmail.com>
2022-07-31 22:31:09 +0900

    Test GCP init(1)

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("/g/내 드라이브/02_개인_TEST/GCP/Keyindra-team-220124-8eab6b03f023.json")
+
+#   project = "<PROJECT_ID>"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
